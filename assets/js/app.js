2e8c52d7992cca9df2f69183aacd07e3d8721c8e
Greg Church <greggypc@users.noreply.github.com>
2017-12-13 15:28:14 -0600

    reading from db

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,6 @@
     losses: 0
   };
 
-
   var p2 = {
     name: '',
     number: 0,
@@ -31,50 +30,30 @@
   };
 
   var turn;
-  //var ppp = 0;
-  var playa1;
-  var playa2;
+  var playa1 = "";
+  var playa2 = "";
 
- $("#playerNameInput").html('<input id="pNameToSubmit" type="text" placeholder="Enter your name" /><input type="button" value="Submit" id="buttonSubmitName" />');
-  $("#buttonSubmitName").click(function() {
-  $("#playerNameInput").hide();
+$("#playerNameInput").html('<div id="submitForm"><input id="pNameToSubmit" type="text" placeholder="Enter your name" /><input type="button" value="Submit" id="buttonSubmitName" /></div>');
+$("#buttonSubmitName").click(function() {
+$("#submitForm").hide();
 
-// --------------------------------------------------------------
-
-// Add ourselves to presence list when online.
-// connectionsRef references a specific location in our database.
-// All of our connections will be stored in this directory.
 var connectionsRef = database.ref("/connections");
-
-// '.info/connected' is a special location provided by Firebase that is updated every time
-// the client's connection state changes.
-// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
 var connectedRef = database.ref(".info/connected");
 
-// When the client's connection state changes...
 connectedRef.on("value", function(snap) {
-
-  // If they are connected..
   if (snap.val()) {
-
-    // Add user to the connections list.
     var con = connectionsRef.push(true);
-
-    // Remove user from the connection list when they disconnect.
     con.onDisconnect().remove();
   }
 });
 
-// When first loaded or when the connections list changes...
 connectionsRef.on("value", function(snap) {
- 
-
-  if (snap.numChildren() === 1) {
+    if (snap.numChildren() === 1) {
     console.log(p1.number + " is the player number - 0 from 1st if statement");
     playa1 = $("#pNameToSubmit").val().trim();
     console.log("player 1 is " + playa1);
     // Uploads player1 game play data to the database - set a new node p1
-    database.ref("/p1").set({
+    database.ref("players/p1").set({
         name: playa1,
         number: 1,
         choice: '',
@@ -82,26 +61,25 @@ connectionsRef.on("value", function(snap) {
         losses: 0
         });
 
-
-  database.ref("/p1").on("child_changed", function(childSnapshot, prevChildKey) {
-    // if (snapshot.child("p1").exists() && snapshot.child("p2").exists()) {
-//console.log(p1.number + " is the player number - 1 from the db");
-    // }
-     var p1Name = childSnapshot.val().name;
-     var p1Number = childSnapshot.val().number;
-    $("#welcomePlayerMessage").html("Welcome " + p1Name + " You are Player " + p1Number);
+    // snap is the value of the ref that changed - here it is name and number
+  database.ref("players/p1").on("value", function(snap) {
+    
+     var p1Name = snap.val().name;
+     var p1Number = snap.val().number;
+     $("#playerNameInput").html("Welcome " + p1Name + ", you are Player " + p1Number);
+     $("#playerOneName").html(p1Name);
+    
   });
-
+  
 // The number of online users is the number of children in the connections list.
   console.log(snap.numChildren() + " connections currently in 1st if");
   } //end first if
   
-
-  else if (snap.numChildren() === 2) {
+else if (snap.numChildren() === 2) {
 playa2 = $("#pNameToSubmit").val().trim();
     console.log("player 2 is " + playa2);
     // Uploads player2 game play data to the database
-    database.ref("/p2").set({
+    database.ref("players/p2").set({
         name: playa2,
         number: 2,
         choice: '',
@@ -110,14 +88,13 @@ playa2 = $("#pNameToSubmit").val().trim();
         });
 
 
-  database.ref("/p2").on("child_changed", function(childSnapshot, prevChildKey) {
-    //if (snapshot.child("p1").exists()) {
-
-    // Set the local variables for highBidder equal to the stored values in firebase.
-    var p2Name = childSnapshot.val().name;
-    var p2Number = childSnapshot.val().number;
-    $("#welcomePlayerMessage").html("Welcome " + p2Name + " You are Player " + p2Number);
-});
+  database.ref("players/p2").on("value", function(snap) {
+    
+     var p2Name = snap.val().name;
+     var p2Number = snap.val().number;
+     $("#playerNameInput").html("Welcome " + p2Name + ", you are Player " + p2Number);
+     $("#playerTwoName").html(p2Name);
+  });
 
   // The number of online users is the number of children in the connections list.
   console.log(snap.numChildren() + " connections currently in else if");
@@ -126,11 +103,7 @@ playa2 = $("#pNameToSubmit").val().trim();
     alert("game is full!");
   };
 
-
-
-
 });  //end connection value listener
-
 	
 });  //end click function on submit name
 
