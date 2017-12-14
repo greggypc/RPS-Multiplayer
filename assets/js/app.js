a5afb8ad059a89767b0f7a124a9dfebdf65db592
Greg Church <greggypc@users.noreply.github.com>
2017-12-13 21:40:08 -0600

    split listener functions out of click function 'submit name'

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,7 @@
   firebase.initializeApp(config);
 
    var database = firebase.database();
-
+   //var newDatabase = new databaseURL("https://rps-multiplayer-b6623.firebaseio.com/");
    var p1 = {
     name: '',
     number: 0,
@@ -33,6 +33,11 @@
   var playa1 = "";
   var playa2 = "";
 
+// $("#reset").on("click", function() {
+//   newDatabase.set({});
+//   window.location.reload();
+// });
+
 $("#playerNameInput").html('<div id="submitForm"><input id="pNameToSubmit" type="text" placeholder="Enter your name" /><input type="button" value="Submit" id="buttonSubmitName" /></div>');
 $("#buttonSubmitName").click(function() {
 $("#submitForm").hide();
@@ -49,7 +54,6 @@ connectedRef.on("value", function(snap) {
 
 connectionsRef.on("value", function(snap) {
     if (snap.numChildren() === 1) {
-    console.log(p1.number + " is the player number - 0 from 1st if statement");
     playa1 = $("#pNameToSubmit").val().trim();
     console.log("player 1 is " + playa1);
     // Uploads player1 game play data to the database - set a new node p1
@@ -61,22 +65,12 @@ connectionsRef.on("value", function(snap) {
         losses: 0
         });
 
-    // snap is the value of the ref that changed - here it is name and number
-  database.ref("players/p1").on("value", function(snap) {
-     
-     var p1Name = snap.val().name;
-     $("#playerNameInput").html("Welcome " + p1Name + ", you are Player 1");
-    
-     $("#playerOneName").html(p1Name);
-    
-  });
-  
 // The number of online users is the number of children in the connections list.
-  console.log(snap.numChildren() + " connections currently in 1st if");
+  console.log(snap.numChildren() + " connections - currently in 1st if");
   } //end first if
   
 else if (snap.numChildren() === 2) {
-playa2 = $("#pNameToSubmit").val().trim();
+    playa2 = $("#pNameToSubmit").val().trim();
     console.log("player 2 is " + playa2);
     // Uploads player2 game play data to the database
     database.ref("players/p2").set({
@@ -87,22 +81,30 @@ playa2 = $("#pNameToSubmit").val().trim();
         losses: 0
         });
 
-  database.ref("players/p2").on("value", function(snap) {
-    
-     var p2Name = snap.val().name;
-     $("#playerNameInput").html("Welcome " + p2Name + ", you are Player 2");
-     $("#playerTwoName").html(p2Name);
-  });
-
   // The number of online users is the number of children in the connections list.
   console.log(snap.numChildren() + " connections currently in else if");
   } //end of else if
   else {
     alert("game is full!");
   };
-
 });  //end connection value listener
-	
 });  //end click function on submit name
 
 
+//================================================================================
+ // snap is the value of the ref that changed - here it is name
+  database.ref("players/p1").once("child_added", function(snap) {
+     
+      var p1Name = snap.val().name;
+     //$("#playerNameInput").html("Welcome " + p1Name + ", you are Player 1");
+     $("#playerOneName").html(p1Name);
+
+  });
+
+//================================================================================
+database.ref("players/p2").once("child_added", function(snap) {
+    
+     var p2Name = snap.val().name;
+     //$("#playerNameInput").html("Welcome " + p2Name + ", you are Player 2");
+     $("#playerTwoName").html(p2Name);
+  });
